73b76c7842140f42dee8016d1c31ef7a0772bb51
Abaci2424 <dymuraoksana@gmail.com>
2022-12-18 21:52:03 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,158 +1,211 @@
 const output = document.querySelector(".output");
-// const allButtons = document.querySelectorAll(".calc_btn");
 const allButtons = document.querySelectorAll(".buttons")[0];
 const history = document.querySelector(".history");
 
-
-// let firstNumber = "";
-// let secondNumber = "";
-
-let withDot = true;
-let operations = []; ['1', '+', '5', '-' , '9']
-let operation = "";
+let operations = [];
 let finish = false;
 
 function clearAll() {
   operations = [];
-  firstNumber = "";
-  secondNumber = "";
-  operation = "";
   finish = false;
-  output.textContent = 0;
+  output.textContent = "0";
   history.textContent = "";
 }
 
+const dot = '.';
 const numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const operationArr = ["-", "+", "*", "÷"];
+const actionArr = ["-", "+", "*", "÷"];
+
+const getLastOperation = () => {
+  return operations[operations.length - 1]
+}
+
+const setLastOperation = (value) => {
+  operations[operations.length - 1] = value
+}
+
+
+const setDefaultOperation = () => {
+  operations = ['0']
+}
+
+const checkIsAction = (value) => {
+  return actionArr.includes(value)
+}
+
+const clearLastAction = () => {
+  const lastOperation = getLastOperation()
+
+  if (checkIsAction(lastOperation)) {
+    operations.pop()
+  }
+}
+
+const formatOperationValue = (value) => {
+  const lastSymbol = value[value.length - 1]
+  if (lastSymbol === dot) {
+    return value.substring(0, value.length - 1)
+  }
+
+  return value
+}
+
+const addDigit = (value) => {
+  if (finish) {
+    clearAll()
+  }
+
+// actionArr.includes(operations[operations.length - 1])
+
+  const lastOperation = getLastOperation()
+
+  if (!lastOperation || checkIsAction(lastOperation)) {
+    operations.push(value);
+  } else {
+    setLastOperation(lastOperation + value)
+  }
+
+  output.textContent = operations.join(" ");
+}
+
+const addAction = (value) => {
+  if (finish) {
+    finish = false
+    history.textContent = ""
+    operations = [output.textContent]
+  }
+
+  clearLastAction()
+
+  operations.push(value)
+
+  output.textContent = operations.join(" ");
+}
+
+const addDot = () => {
+  if (finish) {
+    finish = false
+    history.textContent = ""
+    operations = [output.textContent]
+  }
+
+  clearLastAction()
+
+  const lastOperation = getLastOperation()
+  const existsDot = lastOperation.includes('.')
+
+  if (!existsDot) {
+    setLastOperation(getLastOperation() + dot)
+  }
+
+  output.textContent = operations.join(" ");
+}
+
+const calculate = () => {
+  clearLastAction();
+  history.textContent = operations.join(" ");
+  finish = true;
+
+  const copy = [...operations];
+
+  let i = 1;
+
+  while (true) {
+    if (i >= copy.length) break;
+    const currentOperation = copy[i] // +
+
+    if (!["*", "÷"].includes(currentOperation)) {
+      i += 2;
+      continue;
+    }
+
+    const firstValue = copy[i - 1] // 5
+    const secondValue = copy[i + 1] // 6
+    let res;
+
+    switch (currentOperation) {
+      case "*":
+        res = Number(firstValue) * Number(secondValue)
+        break;
+      case "÷":
+        res = Number(firstValue) / Number(secondValue)
+        break;
+      default:
+        break;
+    }
+
+    copy.splice(i - 1, 3, res)
+  }
+
+  i = 1
+
+  // [25 5/6]
+  while (true) {
+    if (copy.length === 1) break
+    const currentOperation = copy[i] // -
+
+    const firstValue = copy[i - 1] // 25
+    const secondValue = copy[i + 1] // 5/6
+    let res;
+
+    switch (currentOperation) {
+      case "+":
+        res = Number(firstValue) + Number(secondValue)
+        break;
+      case "-":
+        res = Number(firstValue) - Number(secondValue)
+        break;
+      default:
+        break;
+    }
+
+    copy.splice(i - 1, 3, res)
+  }
+
+  output.textContent = copy[0];
+}
 
 allButtons.addEventListener("click", (e) => {
   if (!e.target.classList.contains("calc_btn")) return;
 
   let value = e.target.value;
-  console.log(value);
+
+  // ( 0-9 / . )true
+  if (numbersArr.includes(value)) {
+    addDigit(value)
+
+    return;
+  }
+
+  if (!operations.length) {
+    setDefaultOperation()
+  }
+
+  if (value === '.') {
+    addDot()
+
+    return;
+  }
+
+  // Clear dot in the end
+  const lastOperation = getLastOperation()
+  setLastOperation(formatOperationValue(lastOperation))
 
   if (value === "C") {
     clearAll();
     return;
   }
-  
-  // if (value !== "=") {
-  //   let result = (history.textContent += value);
-  //   console.log(result);
-  // }
-
-  // ( 0-9 / . )true
-  if (numbersArr.includes(value)) {
-    if (finish) {
-      clearAll()
-    }
-
-    if (operation) {
-      operations.push(operation, value);
-    } else if (!operations.length) {
-      operations = [value];
-    } else {
-      operations[operations.length - 1] = withDot ? `${operations[operations.length - 1]}.${value}` : `${operations[operations.length - 1]}${value}`
-    }
-
-    output.textContent = operations.join(" ");
-    operation = ""
-    withDot = false
-
-    return;
-  }
-
-  if (value === '.') {
-    if (finish) {
-      finish = false
-      history.textContent = ""
-      operations = [output.textContent]
-    }
-
-    if(operations.length && !operations[operations.length - 1].includes('.')) {
-      withDot = true
-      output.textContent = operations.join(" ") + '.'
-      operation = ""
-    }
-
-    return;
-  }
 
   // (+, -, ×, /)true
-  if (operationArr.includes(value)) {
-    if (finish) {
-      finish = false
-      history.textContent = ""
-      operations = [output.textContent]
-    }
+  if (actionArr.includes(value)) {
+    addAction(value)
 
-    operation = value;
-    output.textContent = operations.join(" ") + " " + value;
+    return;
   }
 
   // (=)true
   if (value === "=") {
-    history.textContent = operations.join(" ")
-    finish = true;
-    operation = ""
-
-    const copy = [...operations] // [1, +, 24, -, 5/6]
-
-    let i = 1
-
-    while(true) {
-        if(i >= copy.length) break
-        const currentOperation = copy[i] // +
-  
-        if(!["*", "÷"].includes(currentOperation)) {
-          i += 2;
-          continue;
-        }
-        
-        const firstValue = copy[i - 1] // 5
-        const secondValue = copy[i + 1] // 6
-        let res;
-  
-        switch(currentOperation) {
-          case "*":
-            res = Number(firstValue) * Number(secondValue)
-            break;
-          case "÷":
-            res = Number(firstValue) / Number(secondValue)
-            break;
-          default:
-            break;
-        }
-  
-        copy.splice(i - 1, 3, res)
-    }
-
-    i = 1
-
-    // [25 5/6]
-    while(true) {
-      if(copy.length === 1) break
-      const currentOperation = copy[i] // -
-      
-      const firstValue = copy[i - 1] // 25
-      const secondValue = copy[i + 1] // 5/6
-      let res;
-
-      switch(currentOperation) {
-        case "+":
-          res = Number(firstValue) + Number(secondValue)
-          break;
-        case "-":
-          res = Number(firstValue) - Number(secondValue)
-          break;
-        default:
-          break;
-      }
-
-      copy.splice(i - 1, 3, res)
-  }
-
-    output.textContent = copy[0];
+    calculate()
   }
 });
 
